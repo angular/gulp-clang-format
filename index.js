e8f79c06a6889f7ee8aa743623204e209ddc9c20
Alex Eagle <alexeagle@google.com>
2015-03-19 15:00:21 -0700

    Improve output. Instead of printing a line for each file, collect them as we stream and write a single command at the end.

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-var through = require('through2');
-var streamEqual = require('stream-equal');
-var fs = require('fs');
 var clangFormat = require('clang-format');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var streamEqual = require('stream-equal');
+var through = require('through2');
+
+var errors = [];
 
 module.exports = {
   /**
    * Verifies that files are already in the format produced by clang-format.
    * Prints a warning to the console for any file which isn't formatted.
-   * @param {string|Object} options the string "file" to search for a
+   * @param {?(string|Object)} opt_options the string "file" to search for a
    * '.clang-format' file, or an object literal containing clang-format options
    * http://clang.llvm.org/docs/ClangFormatStyleOptions.html#configurable-format-style-options
    */
-  checkFormat: function(options) {
-    options = (typeof options === "undefined") ?
-                  {
-                    BasedOnStyle: "Google"
-                  } :
-                  options;
-    var optsStr =
-        (typeof options === "string") ? options : JSON.stringify(options);
-    return through.obj(function(file, enc, done) {
-      var actual = file.isStream() ?
-                       file.content :
-                       fs.createReadStream(file.path, {encoding: enc});
+  checkFormat: function(opt_options) {
+    var optsStr = opt_options || {BasedOnStyle: "Google"};
+    if (typeof optsStr === "object") {
+      optsStr = JSON.stringify(optsStr);
+    }
 
-      streamEqual(actual,
-                  clangFormat(file, enc, optsStr, done), function(err, equal) {
-                    if (err) {
-                      return done(err);
-                    }
-                    if (!equal) {
-                      console.log("WARNING: " + file.path +
-                                  " is not properly formatted.");
-                      console.log("Please run clang-format -i -style='" +
-                                  optsStr + "' " + file.path);
-                    }
-                    done();
-                  });
-    });
+    return through.obj(
+        function(file, enc, done) {
+          var actualStream =
+              file.isStream() ? file.content : fs.createReadStream(
+                                                   file.path, {encoding: enc});
+          var expectedStream = clangFormat(file, enc, optsStr, done);
+          streamEqual(actualStream, expectedStream, function(err, equal) {
+            if (err) {
+              return done(err);
+            }
+            if (!equal) {
+              errors.push(file.path);
+            }
+            done();
+          });
+        },
+        function(cb) {
+          if (errors.length > 0) {
+            gutil.log("WARNING: Files are not properly formatted. Please run");
+            gutil.log("  clang-format -i -style='" + optsStr + "' " +
+                      errors.join(' '));
+          }
+        });
   }
 };
